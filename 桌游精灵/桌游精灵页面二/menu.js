ec24bdec9baf4b474ca9da5ba149b1e85fc4454c
Ajxvud <2368294530@qq.com>
2021-03-30 18:22:27 +0800

    222

--- 桌游精灵/桌游精灵页面二/menu.js
+++ 桌游精灵/桌游精灵页面二/menu.js
@@ -1,13 +1,6 @@
 
-/* function showDiv(){
-    var menu = document.getElementById("menu");
-    menu.style.display="block";
-}
- */
-
 function doHide(){
     var oDiv = document.getElementById("menu");
-    var oButton = document.getElementById("btnTest")
     if (oDiv.style.display == "none"){
       oDiv.style.display = "block";
     }else {
