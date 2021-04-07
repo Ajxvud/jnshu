2aca9aa221ad0ff18038e2bbfd099d85f5cacf9c
Amon <62141707+Ajxvud@users.noreply.github.com>
2021-04-07 09:10:03 +0800

    add

--- 无用文件/2.js
+++
@@ -1,7 +0,0 @@
-    var menu = document.getElementById('menu');
-	enu.addEventListener('click',
-		function () {
-    	var header = document.getElementsByTagName('header')[0];
-        header.classList.toggle('open');
-    }
-)
