7b785ff72fc56690f8f5ea9ff94eb0b06a458b74
Amon <62141707+Ajxvud@users.noreply.github.com>
2021-04-19 19:39:14 +0800

    add task02_03

--- js任务/task02/js/task.02_04.js
+++ js任务/task02/js/task.02_04.js
@@ -16,4 +16,28 @@ console.log(SS);
 var careerElement = $("#career");
 var numberElement = $("#number");
 var mainBoxElement = $("mainBox");
+//初始化
+
+/* console.log(SS.length); */
+
+for(var i=0; i<SS.length;i++){
+    var num = i+1;
+    //var个变量存储写进文档的标签代码，记住要转义字符
+    var inBox = '<div class=\"mainBox\">\n'+
+                '<div class=\"career\">\n'+ SS[i] +
+                '</div>\n'+
+                '<div class=\"number\">\n'+ num + "号" +
+                '</div>\n'+
+                '</div>\n'+
+                '</div>';
+
+    //通过append将盒子一个一个的往文档里面装           
+    $('.main').append(inBox);
+}
+
+//底部按钮跳转到下一个页面
+$(".foot-button").click(function(){
+    //下一个页面没做好，暂时代替一下
+    location.href = "https://www.baidu.com/";
+})
 
